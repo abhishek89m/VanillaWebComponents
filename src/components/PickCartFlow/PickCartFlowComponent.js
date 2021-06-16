1b63a17fa5c634e125ad8e5c29d2666726b92384
Abhishek <a.mukherjee@adcash.com>
2021-06-16 17:20:46 +0300

    Fixing issues

--- src/components/PickCartFlow/PickCartFlowComponent.js
+++ src/components/PickCartFlow/PickCartFlowComponent.js
@@ -1,30 +1,102 @@
 import { BaseComponent } from '../../utils/BaseComponent.js';
-import CartsService from '../../services/CartsService.js';
+import {
+  createCart,
+  getCarts,
+  deleteCart,
+} from '../../services/CartsService.js';
+import ProductsService from '../../services/ProductsService.js';
 
 export const PickCartFlowIdentifier = 'pick-cart-flow';
 
 export class PickCartFlowComponent extends BaseComponent {
+  products = [];
+
   constructor() {
     super('PickCartFlow', PickCartFlowIdentifier);
+    this.loadProducts();
     this.loadCarts();
   }
 
   async loadCarts() {
-    const carts = await CartsService.getCarts();
+    const carts = await getCarts();
     this.listen('loaded', () => {
       const cartsContainer = this.shadowRoot.querySelector(
         '.pick-cart-flow-container'
       );
 
-      console.log(carts);
-
       carts.forEach(({ id, name }) => {
+        if (!!cartsContainer.querySelector(`#${id}`)) {
+          return;
+        }
+
         const cartItem = this.getCartItemElement();
-        cartItem.id = id;
+        cartItem.querySelector('.cart-item').id = id;
         cartItem.querySelector('.cart-name').innerText = name;
         cartsContainer.appendChild(cartItem);
       });
-    });    
+
+      this.shadowRoot.querySelector('.new-cart')
+        .addEventListener('click', (e) => {
+          this.shadowRoot
+            .querySelector('.add-cart-popup')
+            .classList.toggle('hidden', false);
+        });
+      
+      this.shadowRoot
+        .querySelector('.add-cart-popup')
+        .addEventListener('click', (e) => {
+          if (
+            !e.target.classList.contains('cancel-button') &&
+            !e.target.classList.contains('close-icon') &&
+            !!e.target.closest('.content')
+          ) {
+            return false;
+          }
+
+          e.stopPropagation();
+          this.hideAddModal();
+        });
+
+      this.shadowRoot
+        .querySelector('.save-button')
+        .addEventListener('click', async () => {
+          const { value: cartName } =
+            this.shadowRoot.querySelector('#cart-name');
+
+          const { value: cartDescription } =
+            this.shadowRoot.querySelector('#cart-description');
+
+          createCart(cartName, cartDescription);
+
+          this.hideAddModal();
+        });
+
+      this.shadowRoot
+        .querySelectorAll('.delete-icon')
+        .forEach((node) => {
+          node.addEventListener('click', async (e) => {
+            e.stopPropagation();
+
+            const cartItem = e.target.closest('.cart-item');
+
+            const isDeleted = await deleteCart(cartItem.id);
+            if (isDeleted) {
+              cartItem.remove();
+            }
+          });
+        })
+        
+    });
+  }
+
+  hideAddModal() {
+    this.shadowRoot
+      .querySelector('.add-cart-popup')
+      .classList.toggle('hidden', true);
+  }
+
+  async loadProducts() {
+    this.products = await ProductsService.getProducts();
   }
 
   getCartItemElement() {
@@ -33,6 +105,4 @@ export class PickCartFlowComponent extends BaseComponent {
     );
     return content.cloneNode(true);
   }
-
-  connectedCallback() {}
 }
--- src/services/CartsService.js
+++ src/services/CartsService.js
@@ -1,13 +1,48 @@
-export default {
-  async getCarts() {
-    let carts = localStorage.getItem('carts');
+export async function getCarts() {
+  let carts = localStorage.getItem('carts');
 
-    if (carts === null) {
-      carts = await fetch(`/api/carts.json`).then((response) => response.json());
-      localStorage.setItem('carts', JSON.stringify(carts));
-      return carts;
-    }
+  if (carts === null) {
+    carts = await fetch(`/api/carts.json`).then((response) => response.json());
+    localStorage.setItem('carts', JSON.stringify(carts));
+    return carts;
+  }
 
-    return JSON.parse(carts);
-  },
+  return JSON.parse(carts);
+};
+
+export async function createCart(name, description) {
+  let carts = await getCarts();
+
+  if (carts.some((cart) => cart.name === name)) {
+    // Trying to add the same cart again
+    return;
+  }
+
+  carts.push({
+    name,
+    description,
+    id: `cart-${Date.now()}`, // Using current timestamp as unique ID
+    items: [], // Leaving this empty
+  });
+
+  localStorage.setItem('carts', JSON.stringify(carts));
+};
+
+/**
+ * Returns boolean for whether item was deleted succesfully
+ *
+ * @param {number} cartId 
+ * @returns 
+ */
+export async function deleteCart(cartId) {
+  let carts = await getCarts();
+
+  const updatedCarts = carts.filter((cart) => cart.id !== cartId);
+
+  if (updatedCarts.length !== carts.length) {
+    localStorage.setItem('carts', JSON.stringify(updatedCarts));
+    return true;
+  }
+
+  return false;
 };
